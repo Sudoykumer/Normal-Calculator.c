1808edab7db35652496683328151d775a2d1d23f
Sudoy Kumer Ghosh <sudoyg@gmail.com>
2025-07-18 10:17:49 +0530

    Normal-Calculator.c

--- 12_SwitchCase.c
+++ 12_SwitchCase.c
@@ -1,4 +1,3 @@
-// Program to create a simple calculator
 #include <stdio.h>
 
 int main()
@@ -7,7 +6,7 @@ int main()
     double n1, n2;
 
     printf("Enter an operator (+, -, *, /): ");
-    scanf("%c", &operation);
+    scanf(" %c", &operation);  // Added space to handle newline
     printf("Enter two operands: ");
     scanf("%lf %lf", &n1, &n2);
 
@@ -26,12 +25,14 @@ int main()
         break;
 
     case '/':
-        printf("%.1lf / %.1lf = %.1lf", n1, n2, n1 / n2);
+        if (n2 == 0)
+            printf("Error! Division by zero.");
+        else
+            printf("%.1lf / %.1lf = %.1lf", n1, n2, n1 / n2);
         break;
 
-    // operator doesn't match any case constant +, -, *, /
     default:
-        printf("Error! operator is not correct");
+        printf("Error! Operator is not correct.");
     }
 
     return 0;
